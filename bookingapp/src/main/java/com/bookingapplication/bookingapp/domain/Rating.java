1422d8cfa8051e677dfe52ad9825304d8d41ab4d
andro02 <72816712+andro02@users.noreply.github.com>
2023-11-29 23:52:54 +0100

    Update Rating.java

--- bookingapp/src/main/java/com/bookingapplication/bookingapp/domain/Rating.java
+++ bookingapp/src/main/java/com/bookingapplication/bookingapp/domain/Rating.java
@@ -1,15 +1,9 @@
 package com.bookingapplication.bookingapp.domain;
 
-public enum Rating {
-	
+public enum Rating {	
 	one,
 	two,
 	three,
 	four,
 	five
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 34dd06141f656fd943b96aa48bfc363bcac2f29f
