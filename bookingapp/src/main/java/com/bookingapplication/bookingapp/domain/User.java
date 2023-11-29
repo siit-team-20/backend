2578862555f0b1dea9a6721209777a66ec3b89c7
Andro <andrija.slovic1@gmail.com>
2023-11-29 21:52:01 +0100

    user classes added

--- bookingapp/src/main/java/com/bookingapplication/bookingapp/domain/User.java
+++ bookingapp/src/main/java/com/bookingapplication/bookingapp/domain/User.java
@@ -1,5 +1,85 @@
 package com.bookingapplication.bookingapp.domain;
 
+import rs.ac.uns.ftn.informatika.rest.domain.Greeting;
+
 public class User {
 
+	private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private String address;
+    private String phone;
+    private UserType type;
+
+    public User() {
+
+    }
+    
+    public User(String email, String password, String name, String surname, String address, String phone, UserType type) {
+    	this.email = email;
+    	this.password = password;
+    	this.name = name;
+    	this.surname = surname;
+    	this.address = address;
+    	this.phone = phone;
+    	this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmil(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setType(UserType type) {
+    	this.type = type;
+    }
+    
+    public UserType geType() {
+    	return type;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+	
 }
--- bookingapp/src/main/java/com/bookingapplication/bookingapp/service/UserService.java
+++ bookingapp/src/main/java/com/bookingapplication/bookingapp/service/UserService.java
@@ -1,5 +1,22 @@
 package com.bookingapplication.bookingapp.service;
 
-public class UserService {
+import java.util.Collection;
+
+import com.bookingapplication.bookingapp.domain.User;
+import com.bookingapplication.bookingapp.domain.UserType;
+
+public interface UserService {
+	
+	Collection<User> findAll();
+
+	User findOne(String email);
+	
+	Collection<User> findByType(UserType type);
+
+	User create(User user) throws Exception;
+
+	User update(User user) throws Exception;
+
+	void delete(String email);
 
 }
